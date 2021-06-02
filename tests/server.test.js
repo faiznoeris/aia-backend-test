5f1a71fcf3f0f1944066b561e53099b04650ac73
faiznoeris <m.faiznoeris@gmail.com>
2021-06-02 20:06:49 +0700

    add unit testing using supertest and jest

---
+++ tests/server.test.js
@@ -0,0 +1,23 @@
+const app = require('../server')
+const { flickrService } = require('../services')
+const supertest = require('supertest')
+
+test('GET /flickr-feeds', async () => {
+    const feeds = await flickrService.getFeeds(1, 0)
+    const feed = feeds.items[0]
+
+    await supertest(app)
+        .get('/flickr-feeds')
+        .expect(200)
+        .then(response => {
+
+            // Check type and length
+            expect(Array.isArray(response.body.items)).toBeTruthy()
+            expect(response.body.items.length).toEqual(1)
+
+            // Check data
+            expect(response.body.items[0].title).toBe(feed.title)
+            expect(response.body.items[0].link).toBe(feed.link)
+            expect(response.body.items[0].date_taken).toBe(feed.date_taken)
+        })
+})
