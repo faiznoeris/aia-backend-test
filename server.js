9c7b06063362b185740a1ab7f4f6e2d3c9e56d3b
faiznoeris <m.faiznoeris@gmail.com>
2021-06-03 12:20:56 +0700

    allow cors all origin

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express')
 const routes = require('./routes')
 
 const app = express()
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+
+    next()
+}
+
 app.use(express.json())
+app.use(allowCrossDomain)
 app.use('/', routes)
 
 module.exports = app
